ac0ea8a615e54e73315ffaaae1ba8acf8376d78e
Liuxs <emlliuxiang@126.com>
2019-06-12 09:43:19 +0800

    chapter 20 start, commit to checkout to spike to copy email sending settings

---
+++ functional_tests/test_my_lists.py
@@ -0,0 +1,27 @@
+from .base import FunctionalTest
+from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
+from django.contrib.sessions.backends.db import SessionStore
+from django.conf import settings
+User = get_user_model()
+
+
+class MyListTest(FunctionalTest):
+
+    def create_pre_authenticated_session(self, email):
+        user = User.objects.create(email=email)
+        session = SessionStore()
+        session[SESSION_KEY] = user.pk
+        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+        session.save()
+
+        ## 为了设定cookie，我们要先访问网站
+        ## 而404页面是加载最快的
+        self.browser.get(self.live_server_url + '/404_no_such_url')
+        self.browser.add_cookie(
+            dict(
+                name=settings.SESSION_COOKIE_NAME,
+                value=session.session_key,
+                path='/'
+            )
+        )
+
